46eba2ab625612dab76b9bc72e66605e600e2a40
13gucci <bibabibu0110@gmail.com>
2024-09-14 20:35:27 +0700

    📔 [CABONERF-49] Implement UI Login & Register, and react hook form

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    }
+	base: '/cabonerf-fe-saas/',
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
