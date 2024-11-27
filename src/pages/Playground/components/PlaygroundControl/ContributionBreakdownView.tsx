9a59cc71362df9cd7263c734982ecef3fa23289d
13gucci <bibabibu0110@gmail.com>
2024-11-27 09:58:06 +0700

    🐶 Fix heigth contribution breakdown view

--- src/pages/Playground/components/PlaygroundControl/ContributionBreakdownView.tsx
+++ src/pages/Playground/components/PlaygroundControl/ContributionBreakdownView.tsx
@@ -167,8 +167,8 @@ export default function ContributionBreakdownView({ data }: { data: TransformCon
 	const handleMinimize = () => playgroundControlDispatch({ type: PlaygroundControlDispatch.TRIGGER_MINIMIZE });
 
 	return (
-		<div className="h-auto w-[700px] overflow-scroll pb-3">
-			<div className="sticky left-0 right-0 top-0 flex items-center justify-between border-[0.5px] border-b bg-white p-4">
+		<div className="h-[500px] w-[700px] overflow-scroll pb-3">
+			<div className="sticky left-0 right-0 top-0 z-50 flex items-center justify-between border-[0.5px] border-b bg-white p-4">
 				<div className="flex items-center space-x-2">
 					<ContributeResult w={18} h={18} one="#fb923c" two="#fdba74" />
 					<span className="text-base font-semibold">Impact Assessment Result</span>
